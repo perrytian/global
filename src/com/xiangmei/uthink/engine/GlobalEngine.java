8276c7970de46aaba6e40d51410498d45d81ff96
perrytian <pengyutian@hotmail.com>
2016-03-05 02:08:29 +0800

    更新时间统计信息

--- src/com/xiangmei/uthink/engine/GlobalEngine.java
+++ src/com/xiangmei/uthink/engine/GlobalEngine.java
@@ -2,18 +2,28 @@ package com.xiangmei.uthink.engine;
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
+
 import com.xiangmei.uthink.analysis.Consume;
 import com.xiangmei.uthink.analysis.Output;
 
 public class GlobalEngine {
 
-	
+	private static final Logger logger = Logger.getLogger(GlobalEngine.class); 
 	public static void main(String[] args) {
-		Consume consume = new Consume();
-		consume.saleRedis();
-		StringBuffer sb = consume.memberConsumeInfo();
-		File file = new File("d:\\GlobalConsume.txt");
-		Output.outputAlarm(file, sb, false);
-		consume.freeJedis();
+		try {
+			System.out.println("start");
+			long start = System.currentTimeMillis();
+			Consume consume = new Consume();
+			consume.saleRedis();
+			StringBuffer sb = consume.memberConsumeInfo();
+			File file = new File("d:\\GlobalConsume.txt");
+			Output.outputAlarm(file, sb, false);
+			consume.freeJedis();
+			System.out.println("end -->> "+(System.currentTimeMillis()-start)+"ms");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 }
