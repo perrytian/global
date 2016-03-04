10bc0278ca94add79d7b8bbe1a83621da9a52a27
perrytian <pengyutian@hotmail.com>
2016-03-04 23:41:49 +0800

    消费记录类

---
+++ src/com/xiangmei/uthink/object/SaleRecord.java
@@ -0,0 +1,22 @@
+package com.xiangmei.uthink.object;
+
+public class SaleRecord {
+
+	private String member;
+	private String paid;
+	
+	public String getMember() {
+		return member;
+	}
+	public void setMember(String member) {
+		this.member = member;
+	}
+	public String getPaid() {
+		return paid;
+	}
+	public void setPaid(String paid) {
+		this.paid = paid;
+	}
+	
+	
+}
